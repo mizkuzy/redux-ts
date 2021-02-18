9dc83cd2859bbbb612646e637346cc71adbef45b
lyudmila.rudkova@kandasoft.com <MisteR2901>
2021-02-18 16:53:59 +0300

    add searchRepositories onSubmit

--- src/hooks/useActions.ts
+++ src/hooks/useActions.ts
@@ -1,9 +1,9 @@
 import {useDispatch} from 'react-redux'
 import {bindActionCreators} from 'redux'
-import * as ac from '../state/action-creators'
+import { actionCreators } from '../state';
 
 export const useActions = () => {
   const dispatch = useDispatch()
 
-  return bindActionCreators(ac, dispatch)
+  return bindActionCreators(actionCreators, dispatch)
 }
