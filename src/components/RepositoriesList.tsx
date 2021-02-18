182ed50efc84985adbd4883b16de3c58b3c8b606
lyudmila.rudkova@kandasoft.com <MisteR2901>
2021-02-18 17:38:16 +0300

    show repositories

--- src/components/RepositoriesList.tsx
+++ src/components/RepositoriesList.tsx
@@ -1,9 +1,12 @@
 import {useState} from 'react'
 import {useActions} from '../hooks/useActions'
+import {useTypedSelector} from '../hooks/useTypedSelector'
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState('')
   const {searchRepositories} = useActions()
+
+  const {data, loading, error} = useTypedSelector((state => state.repositories))
 
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -15,6 +18,16 @@ const RepositoriesList: React.FC = () => {
       <input value={term} onChange={e => setTerm(e.target.value)}/>
       <button>Search</button>
     </form>
+    {error && <h3>{error}</h3>}
+    {loading && <h3>Loading...</h3>}
+    {!loading && !error && data.length > 0 &&
+    <div>
+      <h3>Repositories list</h3>
+      <ul>
+        {data.map((name) => <li key={name}>{name}</li>)}
+      </ul>
+    </div>
+    }
   </div>
 }
 export default RepositoriesList
