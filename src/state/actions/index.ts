bc1b422c4f31993160b547aab0a851847685b1ba
lyudmila.rudkova@kandasoft.com <MisteR2901>
2021-02-18 17:16:01 +0300

    remove unnecessary export of interfaces

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,15 +1,15 @@
 import {ActionType} from '../action-types'
 
-export interface SearchRepositoriesAction {
+interface SearchRepositoriesAction {
   type: ActionType.SEARCH_REPOSITORIES
 }
 
-export interface SearchRepositoriesSuccessAction {
+interface SearchRepositoriesSuccessAction {
   type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
   payload?: string[]
 }
 
-export interface SearchRepositoriesErrorAction {
+interface SearchRepositoriesErrorAction {
   type: ActionType.SEARCH_REPOSITORIES_ERROR,
   payload: string
 }
